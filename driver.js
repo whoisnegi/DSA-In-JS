80b45365410e7716e4dbe566336c1a207f254253
Karan Negi <karann@valuefy.com>
2021-10-02 19:05:04 +0530

    added driver code for solving problems on online editors

---
+++ driver.js
@@ -0,0 +1,82 @@
+
+// Driver Code Starts
+'use strict';
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', inputStdin => {
+    inputString += inputStdin
+})
+process.stdin.on('end', _ => {
+    inputString = inputString.trim().split('\n').map(string => {
+        return string.trim();
+    });
+    main();
+})
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+function main() {
+    let t = parseInt(readLine());
+    let i = 0;
+    for (; i < t; i++) {
+        let str = readLine().trim();
+        let obj = new Solution();
+        let res = obj.reverseWord(str);
+        // console.log(res);
+    }
+}
+// Driver Code Ends
+
+class Solution {
+
+    static reverseWord(str) {
+        //Your code starts here
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
