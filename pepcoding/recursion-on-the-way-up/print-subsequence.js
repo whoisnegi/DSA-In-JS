65310e0735be93eda9e4db5bee53d0ae0d61f4dc
Karan Negi <karann@valuefy.com>
2021-10-15 15:32:34 +0530

    added soln for print-subsequence

---
+++ pepcoding/recursion-on-the-way-up/print-subsequence.js
@@ -0,0 +1,52 @@
+
+// Driver Code Starts
+'use strict';
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', inputStdin => {
+    inputString += inputStdin
+})
+process.stdin.on('end', _ => {
+    inputString = inputString.trim().split('\n').map(string => {
+        return string.trim();
+    });
+    main();
+})
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+function main() {
+    let str = readLine();
+    Solution.printSS(str, '');
+}
+// Driver Code Ends
+
+class Solution {
+
+    static printSS(str, res) {
+
+        // Base case
+        if (str.length === 1) {
+            console.log(res + str);
+            console.log(res);
+            return;
+        }
+
+        this.printSS(str.slice(1), res + str[0]);
+        this.printSS(str.slice(1), res);
+
+        return;
+    }
+
+}
+
+console.log(Solution.printSS('abc', ''));
+
+
